7ec1c40f0b8f99bb173c77143b72ed8f86902050
Vyshnavi Konchada <vyshnavikonchada@gmail.com>
2026-01-30 23:48:03 +0530

    backend small modification

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 await connectDB()
 
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
 app.get('/', (req, res)=> res.send("Server is live ... "))
 app.use('/api/users', userRouter);
